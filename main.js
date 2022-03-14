2392911540fc9c2d1d27d33a503fce03d92a4dd4
Cris <mirocristy@gmail.com>
2022-03-14 10:27:38 +0100

    segundo commit

---
+++ main.js
@@ -0,0 +1,5 @@
+alert('Estoy realizando el curso de JavaScript, espero aprender mucho');
+
+document.write('Estoy realizando el curso de JavaScript, espero aprender mucho');
+
+console.log('Estoy realizando el curso de JavaScript, espero aprender mucho');
